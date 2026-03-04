fe4c07d0e93ed39b7ff9b8a56c37044e1d6d14aa
Ismail-1630672 <osman_1997@hotmail.co.uk>
2026-03-04 13:37:59 +0000

    updated version

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,18 +1,23 @@
 terraform {
+
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.26.0"
     }
   }
 
+
+
+
   backend "s3" {
-    bucket = "eks-project-ismail"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
+    bucket       = "ismail-eks-project"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
     use_lockfile = true #important for statelocking to prevent corruption of statefile
-    
+
   }
 }
 
